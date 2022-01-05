14d7db1082e40ae0d846bfc854339b9473d615f4
Robert Evanik <rob.evanik@gmail.com>
2022-01-04 22:18:35 -0500

    Set Up HTML routes

---
+++ routes/html/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/notes', (res, req) => {
+    req.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+router.get('*', (res, req) => {
+    req.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { notes } = require('./db/db.json')
+const apiRoutes = require('./routes/api')
+const htmlRoutes = require('./routes/html')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+
+
+app.listen(PORT, () => console.log(`API server is now on port ${PORT}`))
