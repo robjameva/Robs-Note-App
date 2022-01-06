a5beef6ad165a71351d99f567126a8c4bf67f4dc
Robert Evanik <rob.evanik@gmail.com>
2022-01-06 17:13:56 -0500

    Added uuid package to create unique IDs

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,13 +2,14 @@ const path = require('path');
 const router = require('express').Router();
 const { notes } = require('../../db/db.json')
 const { createNewNote, updateDb } = require('../../lib/notes')
+const { v4: uuidv4 } = require('uuid')
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../db/db.json'));
 });
 
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
+    req.body.id = uuidv4()
 
     const note = createNewNote(req.body, notes);
 
@@ -17,7 +18,7 @@ router.post('/notes', (req, res) => {
 
 
 router.delete('/notes/:id', (req, res) => {
-    const note = notes.find(n => n.id == parseInt(req.params.id));
+    const note = notes.find(n => n.id == req.params.id);
     if (!note) return res.status(404).send('The note with the given ID is not found')
 
     const index = notes.indexOf(note);
